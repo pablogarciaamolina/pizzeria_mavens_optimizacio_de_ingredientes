c5e3cc5742883b25e0941ea5c5a38eeac1548e49
Pablo GM <202110501@alu.comillas.edu>
2022-11-17 22:51:22 +0100

    main

--- PDF/load_stock_html.py
+++ PDF/load_stock_html.py
@@ -1,7 +1,8 @@
 from load_stock_xml import get_data_report_str, weekly_dicts_all_months, _get_dict_stock_as_str_
-import html
+from xhtml2pdf import pisa
 
-def mavens_html(file_name: str, report: str, stock_by_month: dict[str, dict]) -> None:
+def mavens_html(file_name: str, report: str, stock_by_month: dict[str, dict], retorno=False) -> str or None:
+    print('·Creating html file...')
     html_str = ''
     html_str += '<!DOCTYPE html>'
     html_str += '<html>'
@@ -20,7 +21,7 @@ def mavens_html(file_name: str, report: str, stock_by_month: dict[str, dict]) ->
     html_str += '<h2>' + 'Weekly stock by month' + '</h1>'
     html_str += '<p>'
     for month in stock_by_month:
-        html_str += '<br>' + month.upper() + '<br>'
+        html_str += '<br><br>' + month.upper() + '<br>\n'
         html_str += _get_dict_stock_as_str_(stock_by_month[month])
     html_str += '</p>'
     html_str += '</body>'
@@ -30,15 +31,28 @@ def mavens_html(file_name: str, report: str, stock_by_month: dict[str, dict]) ->
     with open('mavens_html.html', 'w', encoding='utf_8') as file:
         file.write(html_str)
 
+    print('<DONE>')
 
+    if retorno: return html_str
 
 def create_html():
     report_str = get_data_report_str('evaluation_readme.txt')
     stock_by_month = weekly_dicts_all_months()
-    print('·Creating html file...')
     mavens_html('mavens_html.html', report_str, stock_by_month)
+    
+def create_pdf_from_html():
+
+    report_str = get_data_report_str('evaluation_readme.txt')
+    stock_by_month = weekly_dicts_all_months()
+    html_str = mavens_html('mavens_html.html', report_str, stock_by_month, retorno=True)
+
+    print('Creating PDF...')
+    #Transform HTML
+    out_file = open('mavens_pdf.pdf', 'w+b')
+    p = pisa.CreatePDF(html_str, out_file)
+    out_file.close()
     print('<DONE>')
 
 if __name__ == '__main__':
 
-    create_html()
+    create_pdf_from_html()
